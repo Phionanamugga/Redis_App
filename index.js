f9a037b91b04c96bef7bca5384f9e8e8f806c7ec
Phiona Namugga <phionanamugga15@gmail.com>
2022-10-22 12:04:57 +0300

    adds caching API requests using Redis

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const axios = require("axios");
+const {getDataFromRedis, setDataInRedis} = require("./config/redis");
+const REDIS_KEY = 'user_data_key';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,10 +14,21 @@ async function fetchUserData(){
 
 async function getUserData(req, res){
     let results;
+    let dataFromCache = false;
     try{
-        results = await fetchUserData();
+        const cachedResults = await getDataFromRedis(REDIS_KEY);
+        if (cachedResults) {
+            dataFromCache = true;
+            results = JSON.parse(cachedResults);
+        } else {
+            results = await fetchUserData();
+            if (results.length == 0) {
+                throw 'API returned empty Data';
+            }
+            await setDataInRedis(REDIS_KEY, results)
+        }
         res.send({
-            dataFromCache: false,
+            dataFromCache: dataFromCache,
             result: results
         });
     }catch (error) {
